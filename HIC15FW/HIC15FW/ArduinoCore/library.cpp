4b70c7fc9b48f9fcf7b6a88ebd7057682fb82c6b
Naomi Wu <sexycyborg@protonmail.ch>
2020-01-13 16:42:54 +0800

    Add files via upload

---
+++ HIC15FW/HIC15FW/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 9/3/2019 11:31:04 AM
+ * Author : 12345
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
